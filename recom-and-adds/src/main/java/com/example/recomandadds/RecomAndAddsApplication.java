93df67cefd7c4fd8495a335fee453172d738aec8
vinaymatta <vinaymatta63@gmail.com>
2022-02-06 02:20:26 +0530

    added recom and adds

---
+++ recom-and-adds/src/main/java/com/example/recomandadds/RecomAndAddsApplication.java
@@ -0,0 +1,13 @@
+package com.example.recomandadds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomAndAddsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomAndAddsApplication.class, args);
+	}
+
+}
---
+++ recom-and-adds/src/test/java/com/example/recomandadds/RecomAndAddsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recomandadds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomAndAddsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
